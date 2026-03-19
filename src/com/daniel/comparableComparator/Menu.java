6facb593156abc123bcb3ccc6808a791d25a6aca
daniel <danielgarciaf07@gmail.com>
2026-03-19 20:16:02 +0100

    menu

--- src/com/daniel/comparableComparator/Menu.java
+++ src/com/daniel/comparableComparator/Menu.java
@@ -1,5 +1,7 @@
 package com.daniel.comparableComparator;
 
+import java.util.ArrayList;
+
 public class Menu {
 
     public static void menu() {
@@ -11,5 +13,19 @@ public class Menu {
 
 
     }
+
+    public static void mostrarTabla(ArrayList<Persona> grupo) {
+        System.out.println("------------------------------------------------------");
+        System.out.printf("%-10s %8s %8s %8s %8s%n",
+                "Nombre", "Chup.", "Cub.", "Cerv.", "Total");
+        System.out.println("------------------------------------------------------");
+
+        for (Persona p : grupo) {
+            System.out.println(p);
+        }
+
+        System.out.println("------------------------------------------------------");
+    }
+
 
 }
